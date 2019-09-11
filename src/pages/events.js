738ab4b0767a62117ff70b6b5515755cf6087ff2
Dan Choiniere <dchoinie@gmail.com>
2019-09-11 12:46:07 -0500

    changed events to tour

--- src/pages/events.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react"
-
-export class events extends Component {
-  render() {
-    return <div>Events</div>
-  }
-}
-
-export default events
---
+++ src/pages/tour.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+export class tour extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default tour
