40f8dd8a876cccc189cd5527049a0e8b0ce39230
Dan Choiniere <dchoinie@gmail.com>
2019-10-03 15:09:41 -0500

    Update carousel.js
    Queried for images through gatsby-image & graphql to increase loading time

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,34 +1,79 @@
-import React, { Component } from "react"
+import React from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from "react-responsive-carousel"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import Otr from "../images/carousel/otr_carousel.jpg"
 import Senorita from "../images/carousel/senorita_carousel.png"
 import Tour from "../images/carousel/bright_lights_tour_carousel.jpg"
 
-class HomeCarousel extends Component {
-  render() {
-    return (
-      <Carousel
-        infiniteLoop={true}
-        autoPlay={true}
-        showThumbs={false}
-        showIndicators={false}
-        showStatus={false}
-        transitionTime={450}
-        interval={4000}
-      >
-        <div>
-          <img src={Tour} />
-        </div>
-        <div>
-          <img src={Otr} />
-        </div>
-        <div>
-          <img src={Senorita} />
-        </div>
-      </Carousel>
-    )
-  }
+const HomeCarousel = () => {
+  const data = useStaticQuery(graphql`
+    {
+      car1: file(
+        relativePath: { eq: "carousel/bright_lights_tour_carousel.jpg" }
+      ) {
+        childImageSharp {
+          fluid(quality: 100) {
+            src
+            srcWebp
+            tracedSVG
+            srcSetWebp
+            base64
+            srcSet
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+      car2: file(relativePath: { eq: "carousel/otr_carousel.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            src
+            srcWebp
+            tracedSVG
+            srcSetWebp
+            base64
+            srcSet
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+      car3: file(relativePath: { eq: "carousel/senorita_carousel.png" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            src
+            srcWebp
+            tracedSVG
+            srcSetWebp
+            base64
+            srcSet
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Carousel
+      infiniteLoop={true}
+      autoPlay={true}
+      showThumbs={false}
+      showIndicators={false}
+      showStatus={false}
+      transitionTime={450}
+      interval={4000}
+    >
+      <div>
+        <Img fluid={data.car1.childImageSharp.fluid} />
+      </div>
+      <div>
+        <Img fluid={data.car2.childImageSharp.fluid} />
+      </div>
+      <div>
+        <Img fluid={data.car3.childImageSharp.fluid} />
+      </div>
+    </Carousel>
+  )
 }
 
 export default HomeCarousel
