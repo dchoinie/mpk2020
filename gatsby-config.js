1f3bf59a695466c5cce361c226beaa5a9ef141bf
Dan Choiniere <dchoinie@gmail.com>
2019-12-09 14:43:53 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,17 +15,13 @@ module.exports = {
       options: {
         trackingId: "UA-58225315-7",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
+        head: true,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "UA-58225315-7",
       },
     },
     `gatsby-plugin-react-helmet`,
