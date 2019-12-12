db2abed9d89d931606d8f61ab4a373e7f2b571ef
Dan Choiniere <dchoinie@gmail.com>
2019-12-12 13:50:42 -0600

    Update kid.js
    Added state to allow for star image to appear when hovering mouse over kid image.

--- src/components/kid.js
+++ src/components/kid.js
@@ -3,34 +3,46 @@ import Img from "gatsby-image"
 import { Link } from "gatsby"
 
 export class kid extends Component {
-  state = {
-    isStarShowing: false,
+  constructor(props) {
+    super(props)
+    this.onMouseOver = this.onMouseOver.bind(this)
+    this.onMouseOut = this.onMouseOut.bind(this)
+    this.state = {
+      isHovered: false,
+      image: this.props.kid.headshot.fluid,
+    }
   }
+
+  onMouseOver() {
+    console.log("Mouse Over")
+    this.setState({
+      isHovered: true,
+      image: this.props.kid.headshotStar.fluid,
+    })
+  }
+
+  onMouseOut() {
+    console.log("Mouse Out")
+    this.setState({
+      isHovered: false,
+      image: this.props.kid.headshot.fluid,
+    })
+  }
+
   render() {
     const { kid } = this.props
     return (
-      <Link to={`/${kid.slug}`} className="black">
-        <div className="kidImageHover">
-          <h2>{kid.name}</h2>
-          <Img fluid={kid.headshotStar.fluid} className="my-2" />
-        </div>
+      <Link
+        to={`/${kid.slug}`}
+        className="black"
+        onMouseEnter={() => this.onMouseOver()}
+        onMouseLeave={() => this.onMouseOut()}
+      >
+        <h2>{kid.name}</h2>
+        <Img fluid={this.state.image} className="my-2" />
       </Link>
     )
   }
 }
 
 export default kid
-
-// const kid = ({ kid }) => {
-//   return (
-//     <Link to={`/${kid.slug}`} className="black">
-//       <div className="kidImageHover">
-//         <h2>{kid.name}</h2>
-//         <Img fluid={kid.headshot.fluid} className="my-2 headshot" />
-//         <Img fluid={kid.headshotStar.fluid} className="my-2 headshotStar" />
-//       </div>
-//     </Link>
-//   )
-// }
-
-// export default kid
