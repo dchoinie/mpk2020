1d7ad57aa8d0c954dbac7fee368c02aa3f2a245c
Dan Choiniere <dchoinie@gmail.com>
2020-02-24 09:58:53 -0600

    Auth0 new app

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,15 +98,15 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-source-cloudinary`,
-    //   options: {
-    //     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    //     apiKey: process.env.CLOUDINARY_API_KEY,
-    //     apiSecret: process.env.CLOUDINARY_API_SECRET,
-    //     resourceType: `image`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+      },
+    },
     // {
     //   resolve: "gatsby-source-shopify",
     //   options: {
