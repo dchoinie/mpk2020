3e82f29bd5b563bddaa99f2593d59f71102d43cc
Dan Choiniere <dchoinie@gmail.com>
2020-06-22 13:30:43 -0500

    Update ep-6.js
    new pop star choreo video

--- src/pages/dance-at-home/ep-6.js
+++ src/pages/dance-at-home/ep-6.js
@@ -79,7 +79,7 @@ const Ep6 = ({ data }) => {
               <iframe
                 width="100%"
                 height="315"
-                src="https://www.youtube.com/embed/O4SZ5hcI9pg"
+                src="https://www.youtube.com/embed/fLZzdKAjusU"
                 frameborder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
